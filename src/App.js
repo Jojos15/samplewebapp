3df5e4b8965e19713bad4aad618626dabf37924c
jojos15 <georgekosmidis1@yahoo.gr>
2020-10-17 18:20:22 +0300

    added basic stracture for landing page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="./">
           <img src={logo} width="30" height="30" className="d-inline-block align-top mr-2" alt="Home" loading="lazy" />
             Pedometer App
         </a>
--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 const LandingScreen = () => {
 
+    const buttonHandler = (e) => {
+        e.preventDefault();
+        console.log("Clicked biatch")
+    };
+
     return (
         <div className="container">
-            <div className="row justify-content-center h-25">
-                <h1>Hey Traveler!</h1>
-            </div>
-            <div className="row justify-content-center h-25">
-                <h5>Please enter your name below to start using the app</h5>
+            <div className="row align-items-center justify-content-center h-100">
+                <div className="row align-self-end">
+                    <h1>Hey fellow walker!</h1>
+                </div>
+                <div className="row">
+                    <h5>Please enter your name below to start using the app</h5>
+                </div>
+                <form onSubmit={buttonHandler} className="row align-self-start justify-content-center w-100">
+                    <div className="col-5 ">
+                        <input className="form-control form-control-lg" type="text" placeholder="Your Amazing Name"></input>
+                    </div>
+                    <div className="col-5">
+                        <button type="submit" className="btn btn-outline-primary btn-lg btn-block" onClick={buttonHandler}>Go!</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
